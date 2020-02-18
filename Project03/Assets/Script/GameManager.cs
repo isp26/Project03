c0928ef56d34d15fa88baf895d09db1a8a03dc58
genesispk <60941516+genesispk@users.noreply.github.com>
2020-02-18 14:55:37 -0500

    Comeplete Project 3
    One long road - Done
    
    Reverse Color change - Done
    - K , L key change the color
    
    Color MAT change - Done
    
    Scores - Done
    
    Speed up - Done
    - Every 500m, speed up
    
    Spwan Random Obstacle - Done
    - Every 5 sec, random obstacles will be spawn. And spawn time will be decreased by -0.01.
    
    Game over plane(Score pops up) - Done
    
    !!!!Caution!!!!
    
    Random Object should be have center based pivot.

--- Project03/Assets/Script/GameManager.cs
+++ Project03/Assets/Script/GameManager.cs
@@ -6,9 +6,44 @@ public class GameManager : MonoBehaviour
     bool gameHasEnded = false;
 
     public float restartDelay = 1f;
-
+    public PlayerMovement movement;
     public GameObject completeLevelUI;
     public GameObject restratLevelUI;
+    public Transform player;
+
+    //Start spawn scripts
+    public GameObject[] objects;                // The prefab to be spawned.     
+    private Vector3 spawnPosition;
+    public float timeBetween = 5f;
+    private float timestamp;
+
+    void Update()
+    {
+        if (Time.time >= timestamp )
+        {
+            Spawn();
+            timestamp = Time.time + timeBetween;
+            if (timeBetween <= 1)
+            {
+                timeBetween = 1;
+            }
+            else
+            {
+                timeBetween -= 0.15f;
+            }
+        }
+    }
+
+
+    void Spawn()
+    {
+        spawnPosition.x = player.position.x + 350;
+        spawnPosition.y = 1;
+        spawnPosition.z = 0;
+
+        Instantiate(objects[UnityEngine.Random.Range(0, objects.Length)], spawnPosition, Quaternion.identity);
+    }
+    //Ends spawn scripts
 
     public void CompleteLevel ()
     {
@@ -19,6 +54,7 @@ public class GameManager : MonoBehaviour
     {
         if (gameHasEnded == false)
         {
+            movement.enabled = false;
             gameHasEnded = true;
             Debug.Log("Game Over");
             Invoke("Restart", restartDelay);
---
+++ Project03/Assets/Script/Score.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    public float playerDistance;
+    public bool speedUpAgain = true;
+    public float playerSpeedUpAgain = 0;
+
+
+    void Update()
+    {
+        scoreText.text = player.position.x.ToString("0");
+        playerDistance = player.position.x + playerSpeedUpAgain;
+
+        if (playerDistance > 500)
+        {
+            playerSpeedUpAgain -= 500;
+            player.GetComponent<PlayerMovement>().forwardForce += 500;
+            Debug.Log("Speed UP!");
+        }
+    }
+
+
+
+}
