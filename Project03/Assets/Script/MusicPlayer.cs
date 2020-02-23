10b177f5a70f4d374d7ce7878e7ca158e5f3ca8f
isp26 <isp26@drexel.edu>
2020-02-23 17:03:28 -0500

    Added music and two other prefabs

---
+++ Project03/Assets/Script/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusicBetweenScenes : MonoBehaviour
+{
+
+    private static PlayMusicBetweenScenes instance = null;
+    public static PlayMusicBetweenScenes Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
